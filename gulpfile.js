26e667cc21697c730f064f7a20e8685e506f2a41
Daniele Esposti <expo@expobrain.net>
2015-07-31 22:26:05 +0100

    Base skeleton

---
+++ gulpfile.js
@@ -0,0 +1,50 @@
+var gulp       = require('gulp'),
+    path       = require('path'),
+    reactify   = require('reactify'),
+    browserify = require('gulp-browserify'),
+    notify     = require('gulp-notify');
+
+
+var paths = {
+  src: path.join(__dirname, 'src/**/*.js'),
+  static: path.join(__dirname, 'static/**'),
+  build: path.join(__dirname, 'build'),
+  dest: path.join(__dirname, 'build/js'),
+  srcRoot: path.join(__dirname, 'src')
+};
+
+
+gulp.task('static', function () {
+  gulp.src(paths.static)
+    .pipe(gulp.dest(paths.build))
+    .pipe(notify({
+      message: 'Static complete!',
+      onLast: true
+    }));
+});
+
+
+gulp.task('scripts', function () {
+  gulp.src(paths.src)
+    .pipe(browserify({
+      transform: [reactify],
+      debug: true,
+      cache: {}, packageCache: {}, fullPaths: true
+    }))
+    .pipe(gulp.dest(paths.dest))
+    .pipe(notify({
+      message: 'Scripts complete!',
+      onLast: true
+    }));
+});
+
+
+gulp.task('build', ['scripts', 'static']);
+
+
+gulp.task('watch', ['build'], function () {
+  gulp.watch(paths.src, ['scripts']);
+});
+
+
+gulp.task('default', ['watch']);
